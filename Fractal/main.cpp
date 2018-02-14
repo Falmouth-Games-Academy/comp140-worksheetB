a6bd0a6609553692e4fa3068f8ab0e61ac0c25e9
dicemaster <dicemaster555@gmail.com>
2018-02-14 15:57:54 +0000

    Implamented the formula to create a mandlebrot fractal but it doesn't seem to work. I feel like this should work, but i have no idea why it doesn't.

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -4,7 +4,7 @@
 
 #include "stdafx.h"
 
-int main(int, char**) 
+int main(int, char**)
 {
 	int windowWidth = 800;
 	int windowHeight = 800;
@@ -64,35 +64,66 @@ int main(int, char**)
 		//Clear the renderer
 		SDL_RenderClear(renderer);
 
-		
+
 		//do drawing here
 		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
 
 
-
 		for (int pixelY = 0; pixelY < windowHeight; pixelY++) {
 			// TODO: Map the y coordinate into the range minY to maxY
-			//double y0 =
-			for (int pixelX = 0; pixelX < windowWidth; pixelX++){
+			double y0 = (pixelY / windowHeight) * (maxY - minY) + minY;
 
+			for (int pixelX = 0; pixelX < windowWidth; pixelX++) {
 				// TODO: Map the x coordinate into the range minX to maxX
-				//double x0 =
+				double x0 = (pixelX / windowWidth) * (maxX - minY) + minX;
 
 				unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
 
 				// TODO: implement the algorithm to colour a single pixel (x0, y0) of the fractal
 				// The code below simply fills the screen with random pixels
 
+				int i = 0;
+				int maxIterations = 50;
+
+				double tempX = 0;
+				double tempY = 0;
+
+				bool isTrue = false;
+
+				while (i < maxIterations)
+				{
+
+					tempY = (2 * tempX * tempY) + y0;
+					tempX = ((tempX * tempX) - (tempY * tempY)) + x0;
+
+					i++;
+					if ((tempX * tempX) + (tempY * tempY) >= 4  && i == 50)
+					{
+						isTrue = true;
+						break;
+					}
+				}
+
+				if (isTrue == true)
+				{
+					Uint32 colour = SDL_MapRGB(pixelFormat, rand() % 255, 0, 0);
+					pixels[pixelPosition] = colour;
+				}
+				else
+				{
+					Uint32 colour = SDL_MapRGB(pixelFormat, 0, 0, rand() % 255);
+					pixels[pixelPosition] = colour;
+				}
+
+
 				// Write the pixel
 				// TODO: change this for desired pixel colour value
-				Uint32 colour = SDL_MapRGB(pixelFormat, rand()%255, rand() % 255, rand() % 255);
 				// Now we can set the pixel(s) we want.
-				pixels[pixelPosition] = colour;
 			}
 		}
 
 		SDL_UnlockTexture(fractalTexture);
-		
+
 		SDL_RenderCopy(renderer, fractalTexture, NULL, NULL);
 		//Display the work the renderer has been doing, this make something appear on the screen
 
