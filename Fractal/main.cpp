f7a840ef43a99cf44f986f5210e06dbf6608363d
ac200905 <ac200905@falmouth.ac.uk>
2018-02-15 15:31:06 +0000

    Got Fractal working.

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -69,25 +69,61 @@ int main(int, char**)
 		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
 
 
+		// Nested For Loop to go through every pixel in the window 
+		for (int pixelY = 0; pixelY < windowHeight; pixelY++) 
+		{
+			// Map the y coordinate into the range minY to maxY
+			double y0 = (pixelY / (double)windowHeight) * (maxY - minY) + minY;
 
-		for (int pixelY = 0; pixelY < windowHeight; pixelY++) {
-			// TODO: Map the y coordinate into the range minY to maxY
-			//double y0 =
-			for (int pixelX = 0; pixelX < windowWidth; pixelX++){
+			for (int pixelX = 0; pixelX < windowWidth; pixelX++)
+			{
+				/* The colour to set all pixels initially */
+				Uint32 colourPixel = SDL_MapRGB(pixelFormat, 200, 200, 0);
 
-				// TODO: Map the x coordinate into the range minX to maxX
-				//double x0 =
+				// Map the x coordinate into the range minX to maxX
+				double x0 = (pixelX / (double)windowWidth) * (maxX - minX) + minX;
 
 				unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
 
 				// TODO: implement the algorithm to colour a single pixel (x0, y0) of the fractal
-				// The code below simply fills the screen with random pixels
 
-				// Write the pixel
-				// TODO: change this for desired pixel colour value
-				Uint32 colour = SDL_MapRGB(pixelFormat, rand()%255, rand() % 255, rand() % 255);
+				int i = 0;
+				/* Number of iterations to go through, the higher the number the more 
+				crisp the fractal image becomes */
+				int numIterations = 100;
+
+				// Set the initial x and y to 0 for the first iteration
+				double lastX = 0;
+				double lastY = 0;
+
+				// While loop to go through
+				while (i < numIterations)
+				{
+					/* Uses x and y values calculated in previous iteration (or 0 if just started)
+					to calculate values needed for this iteration */
+					double thisX = ((lastX * lastX) - (lastY * lastY)) + x0;
+					double thisY = (2 * lastX * lastY) + y0;
+					
+					// Inrease iteration number
+					i++;
+
+					// Fractal computation to determine shape (where next pixel colour is changed)
+					if ((thisX * thisX) + (thisY * thisY) >= 4)
+					{
+						/* Write the pixel. Smoothens the colour on the outside of the fractal, 
+						gradually getting lighter through each iteration as i increases assuming 
+						it passed the if statement*/
+						colourPixel = SDL_MapRGB(pixelFormat, i*255/10, 0, 0);
+						break;
+					}
+
+					// Previous values of x and y to be used in the next iteration
+					lastX = thisX;
+					lastY = thisY;
+				
+				}
 				// Now we can set the pixel(s) we want.
-				pixels[pixelPosition] = colour;
+				pixels[pixelPosition] = colourPixel;
 			}
 		}
 
