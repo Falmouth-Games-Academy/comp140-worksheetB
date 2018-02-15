3e02860ff67d1d12c0bcbc1576cd812e9b8b4eeb
dicemaster <dicemaster555@gmail.com>
2018-02-15 13:04:54 +0000

    The formula now creates a circle.

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -3,11 +3,12 @@
 // http://www.willusher.io/pages/sdl2/
 
 #include "stdafx.h"
+#include <complex>
 
 int main(int, char**)
 {
-	int windowWidth = 800;
-	int windowHeight = 800;
+	double windowWidth = 800;
+	double windowHeight = 800;
 	//Initialise the Video Part of SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		//Print out an error message to the screen if this fails
@@ -67,37 +68,68 @@ int main(int, char**)
 
 		//do drawing here
 		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
+		/*
+		SDL_Window *window;
+		SDL_Renderer *renderer;
+		SDL_CreateWindowAndRenderer(windowWidth, windowHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS, &window, &renderer);
+		SDL_SetWindowPosition(window, 65, 126);
 
 
+		enum { PaletteSize = 32 };
+		int palette[PaletteSize + 1][3];
+		for (int i = 0; i <= PaletteSize; ++i)
+		{
+			palette[i][0] = i < 2 * PaletteSize / 3 ? i * 255 * 3 / (2 * PaletteSize) : 255;
+			palette[i][1] = i < PaletteSize / 3 ? 0 : (i - PaletteSize / 3) * 255 * 3 / (2 * PaletteSize);
+			palette[i][2] = 0;
+		}
+
+		for (int y = 0; y < windowHeight; ++y)
+			for (int x = 0; x < windowWidth; ++x)
+			{
+				std::complex<double> c(0.5 * (x - (windowWidth - windowHeight) / 2) / windowHeight * 4.0 - 2.0, 0.5 * y / windowHeight * 4.0 - 2.0);
+				std::complex<double> z(0.0, 0.0);
+				int cnt = 0;
+				while (cnt < PaletteSize)
+				{
+					z = z * z + c;
+					if (abs(z) > 2.0)
+						break;
+					++cnt;
+				}
+				SDL_SetRenderDrawColor(renderer, palette[cnt][0], palette[cnt][1], palette[cnt][2], 0xff);
+				SDL_RenderDrawPoint(renderer, x, y);
+			} */
+		
 		for (int pixelY = 0; pixelY < windowHeight; pixelY++) {
 			// TODO: Map the y coordinate into the range minY to maxY
 			double y0 = (pixelY / windowHeight) * (maxY - minY) + minY;
 
 			for (int pixelX = 0; pixelX < windowWidth; pixelX++) {
 				// TODO: Map the x coordinate into the range minX to maxX
-				double x0 = (pixelX / windowWidth) * (maxX - minY) + minX;
+				double x0 = (pixelX / windowWidth) * (maxX - minX) + minX;
 
 				unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
 
 				// TODO: implement the algorithm to colour a single pixel (x0, y0) of the fractal
 				// The code below simply fills the screen with random pixels
 
-				int i = 0;
+				int i = 1;
 				int maxIterations = 50;
 
-				double tempX = 0;
-				double tempY = 0;
+				double lastX = x0;
+				double lastY = y0;
 
 				bool isTrue = false;
 
 				while (i < maxIterations)
 				{
 
-					tempY = (2 * tempX * tempY) + y0;
-					tempX = ((tempX * tempX) - (tempY * tempY)) + x0;
+					double tempY = (2 * lastX * lastY) + y0;
+					double tempX = ((lastX * lastX) - (lastY * lastY)) + x0;
 
 					i++;
-					if ((tempX * tempX) + (tempY * tempY) >= 4  && i == 50)
+					if ((tempX * tempX) + (tempY * tempY) >= 4)
 					{
 						isTrue = true;
 						break;
@@ -115,12 +147,12 @@ int main(int, char**)
 					pixels[pixelPosition] = colour;
 				}
 
-
 				// Write the pixel
 				// TODO: change this for desired pixel colour value
 				// Now we can set the pixel(s) we want.
 			}
 		}
+		
 
 		SDL_UnlockTexture(fractalTexture);
 
