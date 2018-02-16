656afc707824dd724146a3c00ba316ac578b27d7
Beren Wallis <32566086+BerenWallis@users.noreply.github.com>
2018-02-16 13:09:50 +0000

    Uploaded finished Fractal
    There will be errors with the code as I could not compile at home, I've tried everything to get it to compile with SDL but it won't.

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -8,155 +8,6 @@ const unsigned int NumberOfIterations = 255;
 const unsigned int windowWidth = 800;
 const unsigned int windowHeight = 800;
 
-Uint32 mandelbrot(double x, double y)
-{
-	double yComponent = y;
-	double xComponent = x;
-
-	for (int i = 0; i < NumberOfIterations; i++)
-	{
-		if (xComponent*xComponent + yComponent*yComponent >= 4)
-		{
-			i = (log(i) / log(255)) * 255;
-			return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGB888), i, 0, i);
-		}
-		double tempYComponent = yComponent;
-		yComponent = (2 * xComponent * yComponent) + y;
-		xComponent = (xComponent*xComponent) - (tempYComponent*tempYComponent) + x;
-	}
-	return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGB888), 50, 0,50);
-}
-
-int main(int, char**) 
-{
-	bool fractal1 = false;
-	//Initialise the Video Part of SDL2
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		//Print out an error message to the screen if this fails
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
-
-	//Creare a 800x800 window with the title Fractal
-	SDL_Window *window = SDL_CreateWindow("Fractal", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
-	if (window == nullptr) {
-		//Print out error if this fails
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-
-	//Create a renderer
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		//Print error and cleanup
-		SDL_DestroyWindow(window);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-	//this stores the pixels retrieved from the texture
-	Uint32 * pixels = NULL;
-	//the pitch is the width of the texture in bytes
-	int pitch;
-	//Create a texture, STREAMING means that we will update the texture 
-	SDL_Texture * fractalTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, windowWidth, windowHeight);
-	//Get the pixel format the texture
-	SDL_PixelFormat * pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGB888);
-
-
-	// Minimum and maximum coordinates for the fractal
-	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
-
-	//Controls the game loop
-	bool quit = false;
-	//Holds events coming from SDL
-	SDL_Event event;
-	//Game Loop, while quit is false
-
-	Uint64 NOW = SDL_GetPerformanceCounter();
-	Uint64 LAST = 0;
-	double seconds = 0;
-	while (!quit)
-	{
-		
-		NOW = SDL_GetPerformanceCounter();
-		LAST = NOW;
-		double deltaTime = (double)((NOW - LAST) * 1000 / SDL_GetPerformanceFrequency()); //gets Deltatime
-		seconds += deltaTime;
-
-
-		//Check for Messages from SDL
-		while (SDL_PollEvent(&event)) {
-			//quit is generated when red cross is clicked
-			if (event.type == SDL_QUIT) {
-				quit = true;
-			}
-		}
-
-		//Clear the renderer
-		SDL_RenderClear(renderer);
-
-		
-		//do drawing here
-		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
-
-
-		if (fractal1) // Mandlebrot Set
-		{
-			for (double pixelY = 0; pixelY < windowHeight; pixelY++)
-			{
-				double yLocation = (pixelY / windowHeight)*(maxY - minY) + minY;
-				for (double pixelX = 0; pixelX < windowWidth; pixelX++)
-				{
-					double xLocation = (pixelX / windowWidth)*(maxX - minX) + minX;
-					unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
-
-
-					
-					// Now we can set the pixel(s) we want.
-					pixels[pixelPosition] = mandelbrot(xLocation, yLocation);
-				}
-			}
-		}
-		else //Dragon curve
-		{
-			DragonCurve CurveObject;
-			if (seconds>3)
-			{
-				seconds = 0;
-				CurveObject.NextIteration();
-			}
-			for (double pixelY = 0; pixelY < windowHeight; pixelY++)
-			{
-				for (double pixelX = 0; pixelX < windowWidth; pixelX++)
-				{
-					unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
-					pixels[pixelPosition] = CurveObject.DrawCurve(pixelX,pixelY);
-
-				}
-			}
-		}
-
-		SDL_UnlockTexture(fractalTexture);
-		
-		SDL_RenderCopy(renderer, fractalTexture, NULL, NULL);
-		//Display the work the renderer has been doing, this make something appear on the screen
-
-
-		SDL_RenderPresent(renderer);
-	}
-
-	//cleanup!
-	SDL_FreeFormat(pixelFormat);
-	SDL_DestroyTexture(fractalTexture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
-
-	return 0;
-}
-
 class DragonCurve
 {
 	const unsigned int LengthOfLine = 5;
@@ -224,12 +75,14 @@ private:
 			}
 		}
 	}
+
 public:
 	Uint32 DrawCurve(int x, int y)
 	{
 		if (curveLoc[x][y])
 		{
 			return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGB888), 0, 0, 0);
+			std::cout << "DrawAt: " << x << " , " << y << std::endl;
 		}
 		return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGB888), 255, 255, 255);
 	}
@@ -246,11 +99,170 @@ public:
 			else if (curve[pos] == 'Y')
 			{
 				curve.erase(pos);
-				curve.insert(pos, "−FX−Y");
+				curve.insert(pos, "-FX-Y");
 				pos += 4;
 			}
 		}
 		setCurveIntoArray();
 	}
 
-};
+}CurveObject;
+
+Uint32 mandelbrot(double x, double y)
+{
+	double yComponent = y;
+	double xComponent = x;
+
+	for (int i = 0; i < NumberOfIterations; i++)
+	{
+		if (xComponent*xComponent + yComponent*yComponent >= 4)
+		{
+			i = (log(i) / log(255)) * 255;
+			return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGB888), i, 0, i);
+		}
+		double tempYComponent = yComponent;
+		yComponent = (2 * xComponent * yComponent) + y;
+		xComponent = (xComponent*xComponent) - (tempYComponent*tempYComponent) + x;
+	}
+	return SDL_MapRGB(SDL_AllocFormat(SDL_PIXELFORMAT_RGB888), 50, 0,50);
+}
+
+int main(int, char**) 
+{
+	bool fractal1 = true;
+	//Initialise the Video Part of SDL2
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		//Print out an error message to the screen if this fails
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return 1;
+	}
+
+	//Creare a 800x800 window with the title Fractal
+	SDL_Window *window = SDL_CreateWindow("Fractal", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		//Print out error if this fails
+		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//Create a renderer
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr) {
+		//Print error and cleanup
+		SDL_DestroyWindow(window);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+	//this stores the pixels retrieved from the texture
+	Uint32 * pixels = NULL;
+	//the pitch is the width of the texture in bytes
+	int pitch;
+	//Create a texture, STREAMING means that we will update the texture 
+	SDL_Texture * fractalTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, windowWidth, windowHeight);
+	//Get the pixel format the texture
+	SDL_PixelFormat * pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGB888);
+
+
+	// Minimum and maximum coordinates for the fractal
+	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
+
+	//Controls the game loop
+	bool quit = false;
+	//Holds events coming from SDL
+	SDL_Event event;
+	//Game Loop, while quit is false
+
+	Uint64 NOW = SDL_GetPerformanceCounter();
+	Uint64 LAST = 0;
+	double seconds = 0;
+	std::string answer;
+	std::cout << "Mandlebrot or Dragon Curve?" << std::endl;
+	std::cin >> answer;
+	if (answer[0] == 'D')
+	{
+		fractal1 = false;
+	}
+	else
+	{
+		std::cout << "unsure of input, showing Mandlebrot" << std::endl;
+	}
+	while (!quit)
+	{
+		
+		NOW = SDL_GetPerformanceCounter();
+		LAST = NOW;
+		double deltaTime = (double)((NOW - LAST) * 1000 / SDL_GetPerformanceFrequency()); //gets Deltatime
+		seconds += deltaTime;
+
+
+		//Check for Messages from SDL
+		while (SDL_PollEvent(&event)) {
+			//quit is generated when red cross is clicked
+			if (event.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+
+		//Clear the renderer
+		SDL_RenderClear(renderer);
+
+		
+		//do drawing here
+		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
+
+
+		if (fractal1) // Mandlebrot Set
+		{
+			for (double pixelY = 0; pixelY < windowHeight; pixelY++)
+			{
+				double yLocation = (pixelY / windowHeight)*(maxY - minY) + minY;
+				for (double pixelX = 0; pixelX < windowWidth; pixelX++)
+				{
+					double xLocation = (pixelX / windowWidth)*(maxX - minX) + minX;
+					unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
+
+
+					
+					// Now we can set the pixel(s) we want.
+					pixels[pixelPosition] = mandelbrot(xLocation, yLocation);
+				}
+			}
+		}
+		else //Dragon curve
+		{
+			if (seconds>3)
+			{
+				seconds = 0;
+				CurveObject.NextIteration();
+			}
+			for (double pixelY = 0; pixelY < windowHeight; pixelY++)
+			{
+				for (double pixelX = 0; pixelX < windowWidth; pixelX++)
+				{
+					unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
+					pixels[pixelPosition] = CurveObject.DrawCurve(pixelX,pixelY);
+
+				}
+			}
+		}
+
+		SDL_UnlockTexture(fractalTexture);
+		
+		SDL_RenderCopy(renderer, fractalTexture, NULL, NULL);
+		//Display the work the renderer has been doing, this make something appear on the screen
+
+
+		SDL_RenderPresent(renderer);
+	}
+
+	//cleanup!
+	SDL_FreeFormat(pixelFormat);
+	SDL_DestroyTexture(fractalTexture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
