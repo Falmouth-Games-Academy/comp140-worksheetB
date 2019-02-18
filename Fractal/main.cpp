3e2428c0453345a32220561f3964778a6ec32588
Jakob Cocking <jc220866@falmouth.ac.uk>
2019-02-18 14:25:30 +0000

    added doubles x0 and y0

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -4,25 +4,29 @@
 
 #include "stdafx.h"
 
+int windowWidth = 800;
+int windowHeight = 800;
 
 int main(int, char**) 
 {
-	int windowWidth = 800;
-	int windowHeight = 800;
 	//Initialise the Video Part of SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		//Print out an error message to the screen if this fails
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+
 		return 1;
 	}
+
 	//Create a 800x800 window with the title Fractal
 	SDL_Window *window = SDL_CreateWindow("Fractal", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
 	if (window == nullptr) {
 		//Print out error if this fails
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
+
 		return 1;
 	}
+
 	//Create a renderer
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (renderer == nullptr) {
@@ -30,14 +34,19 @@ int main(int, char**)
 		SDL_DestroyWindow(window);
 		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
+
 		return 1;
 	}
+
 	//this stores the pixels retrieved from the texture
 	Uint32 * pixels = NULL;
+
 	//the pitch is the width of the texture in bytes
 	int pitch;
+
 	//Create a texture, STREAMING means that we will update the texture 
 	SDL_Texture * fractalTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, windowWidth, windowHeight);
+
 	//Get the pixel format the texture
 	SDL_PixelFormat * pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGB888);
 
@@ -54,9 +63,11 @@ int main(int, char**)
 	while (!quit)
 	{
 		//Check for Messages from SDL
-		while (SDL_PollEvent(&event)) {
+		while (SDL_PollEvent(&event)) 
+		{
 			//quit is generated when red cross is clicked
-			if (event.type == SDL_QUIT) {
+			if (event.type == SDL_QUIT) 
+			{
 				quit = true;
 			}
 		}
@@ -66,22 +77,31 @@ int main(int, char**)
 		//do drawing here
 		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
 
-		for (int pixelY = 0; pixelY < windowHeight; pixelY++) {
+		for (int pixelY = 0; pixelY < windowHeight; pixelY++)
+		{
 			// TODO: Map the y coordinate into the range minY to maxY
 			//double y0 =
-			for (int pixelX = 0; pixelX < windowWidth; pixelX++){
 
+			double y0 = pixelY / windowHeight * (maxY - minY) + minY;
+
+			for (int pixelX = 0; pixelX < windowWidth; pixelX++)
+			{
 				// TODO: Map the x coordinate into the range minX to maxX
 				//double x0 =
+
+				double x0 = pixelX / windowWidth * (maxX - minX) + minX;
 
 				unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
 
 				// TODO: implement the algorithm to colour a single pixel (x0, y0) of the fractal
+
+
 				// The code below simply fills the screen with random pixels
 
 				// Write the pixel
 				// TODO: change this for desired pixel colour value
-				Uint32 colour = SDL_MapRGB(pixelFormat, rand()%255, rand() % 255, rand() % 255);
+				Uint32 colour = SDL_MapRGB(pixelFormat, rand() % 255, rand() % 255, rand() % 255);
+
 				// Now we can set the pixel(s) we want.
 				pixels[pixelPosition] = colour;
 			}
