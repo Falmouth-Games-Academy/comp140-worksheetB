4e879cfb5323d94ba4e2883e0167d3d1e420e145
Jakob Cocking <jc220866@falmouth.ac.uk>
2019-02-20 14:01:32 +0000

    heavily refactored main

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -5,84 +5,94 @@
 #include "stdafx.h"
 #include "main.h"
 
-int WidthOfTextureInBytes;
-
-int windowWidth = 800;
-int windowHeight = 800;
-
-// Minimum and maximum coordinates for the fractal
-const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
-
-//this stores the pixels retrieved from the texture
-Uint32 * pixels = NULL;
-
-//Get the pixel format the texture
-SDL_PixelFormat * pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGB888);
-
-//Holds events coming from SDL
-SDL_Event event;
-
-//Controls the game loop
-bool bWantsToQuit = false;
-
 int main(int, char**) 
 {
-	//Initialise the Video Part of SDL2
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-		//Print out an error message to the screen if this fails
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+	if ( ! Setup() ) { return 0; }
 
-		return 1;
+	while ( ! bWantsToQuit)
+	{
+		while ( SDL_PollEvent( &event ) ) 
+		{
+			CheckForQuit();
+		}
+		
+		GenerateAndRenderFractal();
 	}
 
-	//Create a 800x800 window with the title Fractal
-	// Name, Position (X, Y of top left corner), size and width, is visible(?)
-	SDL_Window *window = SDL_CreateWindow("Fractal", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
-	if (window == nullptr) {
-		//Print out error if this fails
+	SDLCleanUp();
+	return 0;
+}
+
+bool Setup()
+{
+	if (!InitializeVideo()) return false;
+	else if (!CreateWindow()) return false;
+	else if (!CreateRenderer()) return false;
+	else if (!CreateTexture()) return false;
+
+	else return true;
+}
+
+bool InitializeVideo()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+bool CreateWindow()
+{
+	window = SDL_CreateWindow("Fractal", 100, 100, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
 		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-
-		return 1;
+		return false;
 	}
 
-	//Create a renderer
-	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == nullptr) {
-		//Print error and cleanup
+	return true;
+}
+
+bool CreateRenderer()
+{
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr)
+	{
 		SDL_DestroyWindow(window);
 		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
 		SDL_Quit();
-
-		return 1;
+		return false;
 	}
 
+	return true;
+}
+
+bool CreateTexture()
+{
 	//Create a texture, STREAMING means that we will update the texture 
-	SDL_Texture * fractalTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, windowWidth, windowHeight);
-
-	while (!bWantsToQuit)
+	fractalTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, windowWidth, windowHeight);
+	if (fractalTexture == nullptr)
 	{
-		while (SDL_PollEvent(&event)) 
-		{
-			CheckForQuit(event);
-		}
-
-		SDL_RenderClear(renderer);
-		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &WidthOfTextureInBytes);
-		GenerateFractal();
-		SDL_UnlockTexture(fractalTexture);
-		SDL_RenderCopy(renderer, fractalTexture, NULL, NULL);
-		SDL_RenderPresent(renderer);
+		std::cout << "SDL_CreateTexture Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return false;
 	}
 
-	//cleanup!
-	SDL_FreeFormat(pixelFormat);
-	SDL_DestroyTexture(fractalTexture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	return true;
+}
 
-	return 0;
+void GenerateAndRenderFractal()
+{
+	SDL_RenderClear(renderer);
+	SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &WidthOfTextureInBytes);
+	GenerateFractal();
+	SDL_UnlockTexture(fractalTexture);
+	SDL_RenderCopy(renderer, fractalTexture, NULL, NULL);
+	SDL_RenderPresent(renderer);
 }
 
 void GenerateFractal()
@@ -100,7 +110,6 @@ void GenerateFractal()
 			double currentY = 0;
 
 			int currentIteration = 0;
-			int maxIterations = 175;
 
 			double XSquared = 0;
 			double YSquared = 0;
@@ -142,7 +151,7 @@ void GenerateFractal()
 	}
 }
 
-void CheckForQuit(SDL_Event &event)
+void CheckForQuit()
 {
 	switch (event.type)
 	{
@@ -160,4 +169,13 @@ void CheckForQuit(SDL_Event &event)
 		break;
 	}
 	}
+}
+
+void SDLCleanUp()
+{
+	SDL_FreeFormat(pixelFormat);
+	SDL_DestroyTexture(fractalTexture);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
--- Fractal/main.h
+++ Fractal/main.h
@@ -1,5 +1,38 @@
 #pragma once
 
-void CheckForQuit(SDL_Event &event);
+bool Setup();
+bool CreateRenderer();
+bool CreateTexture();
+bool CreateWindow();
+bool InitializeVideo();
+void SDLCleanUp();
+void GenerateAndRenderFractal();
+void CheckForQuit();
+void GenerateFractal();
 
-void GenerateFractal(SDL_PixelFormat * pixelFormat, const double &maxX, const double &minX, const double &maxY, const double &minY, Uint32 * pixels);
+
+int WidthOfTextureInBytes;
+
+int windowWidth = 800;
+int windowHeight = 800;
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+SDL_Texture* fractalTexture = nullptr;
+
+// Minimum and maximum coordinates for the fractal
+const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
+
+//this stores the pixels retrieved from the texture
+Uint32 * pixels = NULL;
+
+//Get the pixel format the texture
+SDL_PixelFormat * pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGB888);
+
+int maxIterations = 175;
+
+//Holds events coming from SDL
+SDL_Event event;
+
+//Controls the game loop
+bool bWantsToQuit = false;
