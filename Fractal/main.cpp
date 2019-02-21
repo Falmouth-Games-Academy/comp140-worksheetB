14065337ed8c006e1273b1abdb6704c8115a37a9
ByronCrowhurst <bc216857@falmouth.ac.uk>
2019-02-21 15:36:17 +0000

    Fractal worksheet complete

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -3,6 +3,24 @@
 // http://www.willusher.io/pages/sdl2/
 
 #include "stdafx.h"
+#include <cstdlib>
+
+int FindMandelbrot(double x0, double y0, int maxi)
+{
+	// Init iterator and x/y values
+	int i = 0;
+	double x = x0, y = y0;
+	// Run Mandelbrot Calc
+	while ((x*x) + (y*y) <= 4 && i < maxi)
+	{
+		double xtemp = (x * x) - (y * y) + x0;
+		y = (2 * x * y) + y0;
+		x = xtemp;
+		i++;
+	}
+	// Return length of iterations
+	return i;
+}
 
 int main(int, char**) 
 {
@@ -42,9 +60,12 @@ int main(int, char**)
 	//Get the pixel format the texture
 	SDL_PixelFormat * pixelFormat = SDL_AllocFormat(SDL_PIXELFORMAT_RGB888);
 
-
 	// Minimum and maximum coordinates for the fractal
 	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
+	// maximum iteration value
+	int maxi = 200;
+	// compare n against for smaller or greater outline to fractal
+	int comparitor = 10;
 
 	//Controls the game loop
 	bool quit = false;
@@ -63,29 +84,31 @@ int main(int, char**)
 
 		//Clear the renderer
 		SDL_RenderClear(renderer);
-
 		
 		//do drawing here
 		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
 
+		for (double pixelY = 0; pixelY < windowHeight; pixelY++) {
+			// Map the y coordinate into the range minY to maxY
+			double y0 = pixelY / windowHeight * (maxY - minY) + minY;
+			for (double pixelX = 0; pixelX < windowWidth; pixelX++){
 
-
-		for (int pixelY = 0; pixelY < windowHeight; pixelY++) {
-			// TODO: Map the y coordinate into the range minY to maxY
-			//double y0 =
-			for (int pixelX = 0; pixelX < windowWidth; pixelX++){
-
-				// TODO: Map the x coordinate into the range minX to maxX
-				//double x0 =
+				// Map the x coordinate into the range minX to maxX
+				double x0 = pixelX / windowWidth * (maxX - minX) + minX;
 
 				unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
 
-				// TODO: implement the algorithm to colour a single pixel (x0, y0) of the fractal
-				// The code below simply fills the screen with random pixels
-
+				// implement the algorithm to colour a single pixel (x0, y0) of the fractal
+				int n = FindMandelbrot(x0, y0, maxi);
 				// Write the pixel
-				// TODO: change this for desired pixel colour value
-				Uint32 colour = SDL_MapRGB(pixelFormat, rand()%255, rand() % 255, rand() % 255);
+				// Should n be greater than comparitor fill in the pixels with colour otherwise fill the pixels black
+				Uint32 colour = SDL_MapRGB(pixelFormat, 0, 0, 0);
+				if (n >= comparitor)
+				{
+					// filled the fractal with my preferred preference of colour.
+					colour = SDL_MapRGB(pixelFormat, 255 - n, 255 - (n/0.5), 255 - n);
+				}
+
 				// Now we can set the pixel(s) we want.
 				pixels[pixelPosition] = colour;
 			}
